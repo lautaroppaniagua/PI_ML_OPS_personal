589295c27c74eb1c9ed58c6ce1bca4c7313e0068
Lautaro Paniagua <lautaroppaniagua@gmail.com>
2023-09-05 05:32:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,8 @@ except AttributeError:
     pass
 else:
     ssl._create_default_https_context = _create_unverified_https_context
-
+    
+nltk.download('wordnet')
 nltk.download('averaged_perceptron_tagger')
 nltk.download('punkt')
 nltk.download('stopwords')
